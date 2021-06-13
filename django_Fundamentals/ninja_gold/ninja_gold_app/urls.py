8fb9dbb59d98e7b9d524f92a1226d071ab2364ed
simonvantulder <vantulder.simon@gmail.com>
2021-06-13 09:35:56 -0700

    update casino & comments

--- django_Fundamentals/ninja_gold/ninja_gold_app/urls.py
+++ django_Fundamentals/ninja_gold/ninja_gold_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('process', views.process),
-    path('reset', views.reset),
+    path('', views.index), # start page
+    path('process', views.process), # process activity
+    path('reset', views.reset), # reset the game
 ]
--- django_Fundamentals/ninja_gold/ninja_gold_app/views.py
+++ django_Fundamentals/ninja_gold/ninja_gold_app/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render, redirect
 import random
 
 def index(request):
-    if "gold_count" in request.session:
+    if "gold_count" in request.session: # display the amount of gold a user has if they have started the game
         print(request.session["gold_count"])
         request.session["winner"] = False
         if "attempts" in request.session:
             request.session['attempts'] +=1
 
     else:
-        request.session["attempts"] = 0
+        request.session["attempts"] = 0 #else display the defaults
         request.session["gold_count"]=10
         request.session["activities"]=[]
 
     return render(request,"index.html")
 
-def process(request):
+def process(request): # depending on the activity selected by the user, displau the resulting gold earned/lost
     if request.POST['box'] =='farm':
         gold_gained = random.randint(10,20)
         request.session['gold_count'] += gold_gained
@@ -35,9 +35,12 @@ def process(request):
     else:
         bet = random.randint(-50, 50)
         request.session['gold_count'] += bet
-        request.session["activities"].append("got {} gold from the casino!".format(bet))
+        if(bet > 0):
+            request.session["activities"].append("won {} gold from the casino!".format(bet))
+        else:
+            request.session["activities"].append("lost {} gold from the casino".format(bet))
         
-    
+    # delete the last entry to prevent stack overflow
     if len(request.session["activities"])>5:
         request.session["activities"].reverse()
         request.session["activities"].pop()    
@@ -45,7 +48,7 @@ def process(request):
 
     return redirect("/")
 
-def reset(request):
+def reset(request): # display gold & attempts
     request.session["gold_count"]=10
     request.session["attempts"] = 0
     return redirect("/")
